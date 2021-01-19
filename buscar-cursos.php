a74e97cc5cef472adcc30bbb3302d969d6bc77f6
GeyssonAguiar <geyssonaguiardemelo@gmail.com>
2021-01-18 22:21:43 -0300

    primeiro commit

---
+++ buscar-cursos.php
@@ -0,0 +1,21 @@
+<?php 
+
+require 'vendor/autoload.php';
+Teste::metodo();
+Teste2::metodo();
+exibeMensagem('aaa');
+exit();
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['verify' => false]);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('https://www.alura.com.br/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . '<br>';
+}
